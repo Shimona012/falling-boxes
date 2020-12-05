deeacec225fd99bdd3c4291373750875916a6e31
Shimona012 <74822423+Shimona012@users.noreply.github.com>
2020-12-05 18:43:22 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,37 @@
+class Ground{
+
+constructor(x,y,width,height){
+
+var groundoptions={
+
+isStatic:true
+
+
+
+}
+
+
+
+this.body=Bodies.rectangle(x,y,width,height,groundoptions)
+this.width=width;
+this.height=height;
+World.add(world,this.body);
+
+
+}
+
+
+display(){
+
+
+    var pos=this.body.position;
+rectMode(CENTER);
+fill("green");
+rect(pos.x,pos.y,this.width,this.height);
+
+
+
+
+}
+
+}
